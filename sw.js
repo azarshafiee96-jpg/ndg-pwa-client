5e46190e8aa45a1cd1a412a6915974e5d19afaa4
azarshafiee96-jpg <azar.shafiee.96@gmail.com>
2025-11-29 20:17:01 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
-const CACHE_NAME = 'ndg-cache-v1';
-const ASSETS = [];
+const CACHE_NAME = "ndg-cache-v1";
 
-self.addEventListener('install', event => {
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/icons/icon-maskable.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
